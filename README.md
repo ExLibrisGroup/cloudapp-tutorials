75e199ab52e2c8da1aae8353266487a2f59a7e7e
Josh Weisman <josh.m.weisman@gmail.com>
2020-05-20 13:43:41 +0300

    Update Readme and Manifest

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Cloud Apps Tutorials
 
 This repository contains supporting code for the [Ex Libris Cloud Apps](https://developers.exlibrisgroup.com/cloudapps/) [tutorials](https://developers.exlibrisgroup.com/cloudapps/tutorials/).
+
+## Contributions
+
+Found a bug? See something we can do better? We are happy to take contributions. You can open an issue in Github, or even better, open a pull request.
--- cloudapp/src/app/parallel/parallel.component.ts
+++ cloudapp/src/app/parallel/parallel.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CloudAppRestService, RestErrorResponse } from '@exlibris/exl-cloudapp-angular-lib';
-import { mergeMap, map, catchError, switchMap } from 'rxjs/operators';
-import { from, of, forkJoin, Observable } from 'rxjs';
+import { map, catchError, switchMap, tap } from 'rxjs/operators';
+import { of, forkJoin, Observable } from 'rxjs';
 import { AppService } from '../app.service';
 
 @Component({
@@ -13,6 +13,8 @@ export class ParallelComponent implements OnInit {
   users: any[];
   num = 10;
   loading = false;
+  processed = 0;
+  showProgress = false;
 
   constructor( 
     private restService: CloudAppRestService,
@@ -30,14 +32,10 @@ export class ParallelComponent implements OnInit {
 
   loadUsers() {
     this.loading = true;
+    this.processed = 0;
     this.restService.call(`/users?limit=${this.num}`)
     .pipe(
-      map(users=>
-        this.addErrors(users.user)
-        .map(user=>withErrorChecking(
-          this.restService.call(`/users/${user.primary_id}?expand=fees`)
-        ))
-      ),
+      map(users=>this.addErrors(users.user).map(user=>this.getUser(user))),
       switchMap(reqs=>forkJoin(reqs)),
     )
     .subscribe({
@@ -54,15 +52,28 @@ export class ParallelComponent implements OnInit {
     });
   }
 
+  clear() {
+    this.users = [];
+  }
+
+  getUser(user: any) {
+    return this.restService.call(`/users/${user.primary_id}?expand=fees`).pipe(
+      tap(()=>this.processed++),
+      catchError(e => of(e)),
+    )
+  }
+
   addErrors(users: any[]) {
     for (let i=0; i<Math.floor(users.length*.25); i++) {
       users.splice(getRandomInt(users.length-1), 0, { primary_id: getRandomInt(1000000) });
     };
     return users;
   }
+
+  get percentComplete() {
+    return Math.round((this.processed/this.num)*100)
+  }
 }
 
 const getRandomInt = (max: number)  => Math.floor(Math.random() * Math.floor(max));
 const isRestErrorResponse = (object: any): object is RestErrorResponse => 'error' in object;
-const withErrorChecking = (obs: Observable<any>): Observable<any> => 
-  obs.pipe(catchError( e => of(e)));
