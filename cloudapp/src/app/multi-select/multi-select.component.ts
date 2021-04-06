7d604748e5aca543f853407b4c907ac680c3ccf2
Josh Weisman <josh.m.weisman@gmail.com>
2021-04-06 16:52:42 +0300

    Use 1.1.0 of select-entities

--- cloudapp/src/app/multi-select/multi-select.component.ts
+++ cloudapp/src/app/multi-select/multi-select.component.ts
@@ -8,27 +8,14 @@ import { CloudAppEventsService, Entity } from '@exlibris/exl-cloudapp-angular-li
   styleUrls: ['./multi-select.component.scss']
 })
 export class MultiSelectComponent implements OnInit {
-  public entities$ = this.eventsService.entities$;
 
   constructor(
     private appService: AppService,
-    private eventsService: CloudAppEventsService
   ) { }
 
   ngOnInit() {
     this.appService.setTitle('Multi-select');
   }
 
-  ngOnDestroy(): void {
-  }
-
-  selectedEntities = new Set<string>();
-  isEntitySelected = (entity: Entity) => this.selectedEntities.has(entity.id);
-
-  onEntitySelected(event: {entity: Entity, checked: boolean}) {
-    if (event.checked) this.selectedEntities.add(event.entity.id);
-    else this.selectedEntities.delete(event.entity.id);
-  }
-
-
+  selectedEntities = new Array<Entity>();
 }
